263a9e965169f1962d334065ba07561b080b4057
Paulo <paulo.rfm.edu@gmail.com>
2021-03-22 00:55:56 -0300

    implementando lista linear estatica

--- menu.c
+++ menu.c
@@ -2,8 +2,6 @@
 int menu_home(){
     printf("\n\tMENU INICIAL");
     printf("\n(1) Lista estatica");
-    printf("\n(2) Lista simplesmente encadeada");
-    printf("\n(3) Lista duplamente encadeada");
     printf("\n(4) Lista circular simplemente encadeada");
     printf("\nEntre com uma opcao: ");
 return 0;
--- opcoes_estruturas.c
+++ opcoes_estruturas.c
@@ -15,21 +15,9 @@ int main(){
             case 1:
                 controller_LLE();
                 break;
-            case 2: 
-                //inserir_final(&ptrLista, pedir_letra());
-                break;
-            case 3:
-                //remover_inicio(&ptrLista);
-                break;
             case 4:
                 controller_LCSE();
                 break; 
-            case 5:
-                //mostrar_lista();
-                break;
-            case 6:
-                //mostra_vogais();
-                break;
             default:
                 break;
             }
