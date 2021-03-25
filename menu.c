d5158647e61acf5db27bede760906e34a77e5cec
Paulo <paulo.rfm.edu@gmail.com>
2021-03-25 18:09:02 -0300

    menu inicial

--- menu.c
+++ menu.c
@@ -3,7 +3,9 @@ int menu_home(){
     printf("\n\tMENU INICIAL");
     printf("\n(1) Lista estatica");
     printf("\n(2) Lista simplesmente encadeada");
+    printf("\n(3) Lista duplamente encadeada");
     printf("\n(4) Lista circular simplemente encadeada");
+    printf("\n(5) Pilha linear estatica");
     printf("\nEntre com uma opcao: ");
 return 0;
 }
